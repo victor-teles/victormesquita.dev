d610aa30ae0612747ad350737da607d902c6a432
Victor <78874691+victor-teles@users.noreply.github.com>
2024-11-06 14:48:47 -0300

    removed thirdParty posts

--- app/lib/get-posts.ts
+++ app/lib/get-posts.ts
@@ -6,43 +6,18 @@ import { cache } from 'react';
 import type { Post } from './types';
 
 const thirdPartyPosts: Post[] = [
-  {
-    title: 'Introducing AI SDK 3.0 with Generative UI support',
-    description: 'Stream React Components from LLMs to deliver richer user experiences.',
-    body: '',
-    date: '2024-03-01T10:00:00.000Z',
-    slug: '',
-    tags: [],
-    lastModified: 0,
-    isThirdParty: true,
-    href: 'https://vercel.com/blog/ai-sdk-3-generative-ui',
-    type: 'post',
-  },
-  {
-    title: 'Introducing the Vercel AI SDK',
-    description:
-      'An interoperable, streaming-enabled, edge-ready software development kit for AI apps built with React and Svelte.',
-    body: '',
-    date: '2023-06-15T13:00:00.000Z',
-    slug: '',
-    tags: [],
-    lastModified: 0,
-    isThirdParty: true,
-    href: 'https://vercel.com/blog/introducing-the-vercel-ai-sdk',
-    type: 'post',
-  },
-  {
-    title: 'Improving the accessibility of our Next.js site',
-    description: "We've made some improvements to the accessibility of our Next.js site. Here's how we did it.",
-    body: '',
-    date: '2022-09-30T13:00:00.000Z',
-    slug: '',
-    tags: [],
-    lastModified: 0,
-    isThirdParty: true,
-    href: 'https://vercel.com/blog/improving-the-accessibility-of-our-nextjs-site',
-    type: 'post',
-  },
+  // {
+  //   title: 'Introducing AI SDK 3.0 with Generative UI support',
+  //   description: 'Stream React Components from LLMs to deliver richer user experiences.',
+  //   body: '',
+  //   date: '2024-03-01T10:00:00.000Z',
+  //   slug: '',
+  //   tags: [],
+  //   lastModified: 0,
+  //   isThirdParty: true,
+  //   href: 'https://vercel.com/blog/ai-sdk-3-generative-ui',
+  //   type: 'post',
+  // },
 ];
 
 export const getPosts = cache(async (includeThirdPartyPosts?: boolean) => {
